737a8ecd5df76e443ba6f93a082aaad34dd6ace1
Mohamed Sorour <110028481+mohamedsorour1998@users.noreply.github.com>
2022-12-24 10:35:03 +0200

    Update s_8.sh

--- Lab2/s_8.sh
+++ Lab2/s_8.sh
@@ -2,11 +2,11 @@
 
 echo "Enter user name"
 read name
-if [ `grep -w $name /etc/passwd` ]
+if [ `grep -w $name /etc/passwd | wc -l ` -ge 1 ]
 then
-	ls -l `grep -w $name /etc/passwd | cut-d: -f6`
-	ps -u $usrName
-	cp -r `grep -w $name /etc/passwd | cut-d: -f6` /temp
+	ls -l `grep -w $name /etc/passwd | cut -d: -f6`
+	ps -u $name 
+	cp -r `grep -w $name /etc/passwd | cut -d: -f6` /temp
 	else
 	echo "this user does not exsit"
 fi
